588479f8735110530f2d23d2320f235fe17c74d2
ryan-munger <112397901+ryan-munger@users.noreply.github.com>
2023-03-25 19:38:01 -0400

    pastrivk

--- bullet_fighter/static/game_logic/collision.js
+++ bullet_fighter/static/game_logic/collision.js
@@ -1,7 +1,11 @@
 
 function detectEnemyCollision(player, enemyList) {
     for (let i = 0; i < enemyList.length; i++) {
-        if (((enemyList[i].x >= player.x) && (enemyList[i].x <= player.x + player.image.width)) && ((enemyList[i].y >= player.y) && (enemyList[i].y <= player.y + player.image.height))) {
+        if (((enemyList[i].x <= player.x && player.x <= enemyList[i].x + enemyList[i].image.width) && (enemyList[i].y <= player.y && player.y <= enemyList[i].y + enemyList[i].image.height)) || // corner one
+            ((enemyList[i].x <= player.x && player.x <= enemyList[i].x + enemyList[i].image.width) && (enemyList[i].y <= player.y + player.image.height && player.y + player.image.height <= enemyList[i].y + enemyList[i].image.height)) || // corner two
+            ((enemyList[i].x <= player.x + player.image.width && player.x + player.image.width <= enemyList[i].x + enemyList[i].image.width) && (enemyList[i].y <= player.y && player.y <= enemyList[i].y + enemyList[i].image.height)) || // corner three
+            ((enemyList[i].x <= player.x + player.image.width && player.x + player.image.width <= enemyList[i].x + enemyList[i].image.width) && (enemyList[i].y <= player.y + player.image.height && player.y + player.image.height <= enemyList[i].y + enemyList[i].image.height))) // corner four
+        {
             return true;
         }
     }

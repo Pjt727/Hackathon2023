9595c4c9bd05a47ea8a1e6f5f0d0905a0e88f3d3
Patrick <76497944+Pjt727@users.noreply.github.com>
2023-03-25 14:43:49 -0400

    Genimg integration

--- bullet_fighter/game/views.py
+++ bullet_fighter/game/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
-
+from .forms import Background
+from image_generator import imageGen
 
 def play(request: HttpRequest, level: int, id:int):
-    return render(request, "play.html", context={'level': level, 'id': id})
+    
+    if request.method == "POST":
+
+        themeForm=Background(request.POST)
+        if themeForm.is_valid():
+            theme = themeForm.cleaned_data['theme']
+            background = imageGen.callImageGenerator(theme)
+    
+    themeForm = Background()
+
+    return render(request, "play.html", context={'level': level, 'id': id, 'themeForm': themeForm, 'background': background})

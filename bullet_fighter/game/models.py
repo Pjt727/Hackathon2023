21439c4672a5dbdf5af90543b1d08f93589479bf
Patrick <76497944+Pjt727@users.noreply.github.com>
2023-03-25 17:47:17 -0400

    update to model

--- bullet_fighter/game/models.py
+++ bullet_fighter/game/models.py
@@ -2,9 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Level(models.Model):
-    url = models.CharField()
+    url = models.CharField(max_length=1000)
 
 
 class Score(models.Model):
+    score = models.IntegerField()
+
     user =  models.OneToOneField(User, on_delete=models.CASCADE, related_name='scores')
-    level = models.ForeignKey(Level, related_name="scores")
+    level = models.ForeignKey(Level, on_delete=models.CASCADE, related_name="scores")
+    
